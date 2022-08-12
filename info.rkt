b1fe30472c67c3525acd74379517f12d0ba35160
Jens Axel Søgaard <jensaxel@soegaard.net>
2022-08-12 15:12:40 +0200

    Add info file

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang info
+
+(define collection      "super")
+(define version         "1.0")
+(define test-omit-paths '("test-object-notation.rkt" "test-ref-expressions.rkt" "test-base.rkt"))
+
+(define deps '("base"))
+
+
